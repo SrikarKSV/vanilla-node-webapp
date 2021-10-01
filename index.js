fb9e3f7b7aaf617c43aacee8ca0a37edb22ff0b0
Srikar KSV <srikar1awesome@gmail.com>
2021-10-01 21:26:49 +0530

    📦 NEW: Added todos for the development of the project

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const http = require('http');
 
+// TODO: Make a logger
+// TODO: Learn to use headers to redirect users
+// TODO: Use javascript to give flashes
+// TODO: Landing page which will have latest 5 confessions
+// TODO: Confessions page with pagination
+// TODO: Confession should be accompanied by title, date and beginning of confession
+// TODO: Use pug and render error.stack if error or the html
+// *: Try authentication for moderator to delete or edit the confession
+
 const app = http.createServer((req, res) => {
+  if (req.url === '/favicon.ico') return;
+  console.log(req.headers);
   res.end('Hello world!!');
 });
 

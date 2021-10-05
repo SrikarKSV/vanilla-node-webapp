a744da7c20d3eeecb5cf160bd968377143ac4004
Srikar KSV <srikar1awesome@gmail.com>
2021-10-05 19:19:30 +0530

    👌 IMPROVE: Improved error handling response

--- app.js
+++ app.js
@@ -7,22 +7,23 @@ const onFinished = require('on-finished');
 const morgan = require('./lib/morgan');
 const { ifRequestIsFile } = require('./lib/utils');
 const render = require('./lib/renderPug');
+const ErrorResponse = require('./lib/errorResponse');
+const { globalErrorHandler } = require('./helpers/errorHandlers');
 
 const serve = serveStatic(path.join(__dirname, 'public'));
 
 const app = http.createServer(server);
 
-// TODO: Learn about on-headers and on-finished for logger
 function server(req, res) {
   const startTime = process.hrtime(); // To calculate response time
   res.render = render;
-  res.on('error', (statusCode, err) => {
-    res.statusCode = statusCode;
-    res.render('error', { err: err });
-  });
+  res.on('error', (err) => globalErrorHandler(err, req, res));
   if (ifRequestIsFile(req)) {
     serve(req, res, () =>
-      res.emit('error', 404, 'Cannot get the requested file!')
+      res.emit(
+        'error',
+        new ErrorResponse(`${req.headers.host}${req.url} does not exist!`, 500)
+      )
     );
   }
 
---
+++ helpers/errorHandlers.js
@@ -0,0 +1,29 @@
+function sendErrorDev(err, req, res) {
+  err.stack = err.stack || '';
+  const errorDetails = {
+    message: err.message,
+    status: err.statusCode,
+    title: 'Something went wrong!',
+    stackHighlighted: err.stack.replace(
+      /[a-z_-\d]+.js:\d+:\d+/gi, // This is to highlight the filenames from which the error orginated
+      '<mark>$&</mark>'
+    ),
+  };
+  res.statusCode = err.statusCode;
+  res.render('error', errorDetails);
+}
+
+function sendErrorProd(err, req, res) {
+  res.statusCode = err.statusCode;
+  res.render('error', {
+    message: err.message,
+    status: err.statusCode,
+    title: 'Something went wrong!',
+  });
+}
+
+exports.globalErrorHandler = (err, req, res) => {
+  err.statusCode = err.statusCode || 500;
+  if (process.env.NODE_ENV === 'development') sendErrorDev(err, req, res);
+  else if (process.env.NODE_ENV === 'production') sendErrorProd(err, req, res);
+};

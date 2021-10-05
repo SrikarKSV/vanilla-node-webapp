36d094636f01286e42e309e033c9c4b4facac850
Srikar KSV <srikar1awesome@gmail.com>
2021-10-05 18:33:01 +0530

    👌 IMPROVE: Improved logging with on-finished package

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const http = require('http');
 const path = require('path');
 const serveStatic = require('serve-static');
 const finalhandler = require('finalhandler');
+const onFinished = require('on-finished');
+
 const morgan = require('./lib/morgan');
 const { ifRequestIsFile } = require('./lib/utils');
 const render = require('./lib/renderPug');
 
 const serve = serveStatic(path.join(__dirname, 'public'));
 
-// TODO: Complete error handling with eventemitter
 const app = http.createServer(server);
 
+// TODO: Learn about on-headers and on-finished for logger
 function server(req, res) {
   const startTime = process.hrtime(); // To calculate response time
   res.render = render;
@@ -19,14 +21,14 @@ function server(req, res) {
     res.render('error', { err: err });
   });
   if (ifRequestIsFile(req)) {
-    return serve(req, res, () =>
+    serve(req, res, () =>
       res.emit('error', 404, 'Cannot get the requested file!')
     );
   }
 
-  res.render('index', { name: 'Srikar' });
+  if (req.url === '/') res.render('index', { name: 'Srikar' });
 
-  morgan.dev(req, res, startTime);
+  onFinished(res, () => morgan.dev(req, res, startTime));
 }
 
 module.exports = app;

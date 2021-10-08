a6a935af0ddd005d300a166faaa076c529540bdd
Srikar KSV <srikar1awesome@gmail.com>
2021-10-08 08:51:20 +0530

    👌 IMPROVE: Fixed new route to accept valid URL

--- routes/homeRouter.js
+++ routes/homeRouter.js
@@ -8,12 +8,8 @@ function router(req, res) {
   switch (httpMethod) {
     case 'GET':
       if (URL === '/') homeController.home(req, res);
-      if (URL === '/new') {
-      }
-      break;
-    case 'POST':
-      if (URL === '/new') {
-      }
+      else if (URL.match(/\/new(\/)?/)) homeController.newConfession(req, res);
+      // TODO: Add edit route if admin functionality is added
       break;
     default:
       const errorMessage = `${httpMethod} is not ALLOWED on ${req.headers.host}${URL}`;

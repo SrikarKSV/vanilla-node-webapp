3d8fdb82915ed59e591cc1f0e876a9eb19e53f26
Srikar KSV <srikar1awesome@gmail.com>
2021-10-08 09:00:10 +0530

    📦 NEW: Added mongoose setup to connect to mongodb

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
+require('dotenv').config();
 const app = require('./app');
+const mongoose = require('mongoose');
+
+const DB =
+  process.env.NODE_ENV === 'development'
+    ? process.env.DATABASE_DEV
+    : process.env.DATABASE_PROD.replace(
+        '<PASSWORD>',
+        process.env.DATABASE_PASSWORD
+      );
+
+mongoose
+  .connect(DB)
+  .then(() => console.log('✅✅ Connected to DATABASE'))
+  .catch((err) =>
+    console.log('💥💥 There was an error connecting to DATABASE', err)
+  );
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>

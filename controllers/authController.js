92444958f6f8a024bfd09b2a441b93003ab65813
Srikar KSV <srikar1awesome@gmail.com>
2021-10-22 10:57:34 +0530

    📦 NEW: Setup signup process

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,31 @@
+const parse = require('co-body');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const maxAge = 3 * 24 * 60 * 60;
+function createToken(id) {
+  return jwt.sign({ id }, process.env.SECRET, { expiresIn: maxAge });
+}
+
+function createSendToken(user, res) {
+  const token = createToken(user._id);
+
+  res.writeHead(303, {
+    location: `/`,
+    'Set-Cookie': `jwt=${token}; Max-Age=${maxAge * 1000}; HttpOnly`,
+  });
+  res.end();
+}
+
 exports.login = (req, res) => {
   res.end('POST - Login');
 };
 
-exports.signup = (req, res) => {
-  res.end('POST - Sign up');
+exports.signup = async (req, res) => {
+  const { username, password, role } = await parse.form(req);
+
+  const user = await User.create({ username, password, role });
+  createSendToken(user, res);
 };
 
 exports.logout = (req, res) => {
@@ -15,5 +37,5 @@ exports.getLogin = (req, res) => {
 };
 
 exports.getSignup = (req, res) => {
-  res.end('GET - Sign up');
+  res.render('signup');
 };

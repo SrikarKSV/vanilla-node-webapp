b358488d213c18150450950336a04e16f02e719b
Srikar KSV <srikar1awesome@gmail.com>
2021-10-24 21:03:01 +0530

    📦 NEW: Added edit controllers

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const parse = require('co-body');
 const Confession = require('../models/Confession');
 const User = require('../models/User');
 
@@ -16,4 +17,35 @@ exports.profile = async (req, res) => {
     .populate('postsMarked')
     .populate('postsEdited');
   res.render('profile', { user });
+};
+
+exports.getEdit = async (req, res) => {
+  const id = path.parse(req.url).name;
+  const { title, confession } = await Confession.findById(id);
+  res.render('edit-confession', {
+    id,
+    title,
+    confession,
+  });
+};
+
+exports.edit = async (req, res) => {
+  const id = path.parse(req.url).name;
+  const { title, confession } = await parse.form(req);
+
+  const editedConfession = await Confession.findByIdAndUpdate(id, {
+    title,
+    confession,
+    editedByStaff: res.locals.user._id,
+  });
+
+  await User.findByIdAndUpdate(res.locals.user._id, {
+    $push: { postsEdited: editedConfession._id },
+  });
+
+  res
+    .writeHead(303, {
+      location: `/confessions/${editedConfession.slug}`,
+    })
+    .end();
 };

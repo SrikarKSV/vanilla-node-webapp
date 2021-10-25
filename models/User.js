ca8e79e5895db3440ed9741b341b00f8f7347776
Srikar KSV <srikar1awesome@gmail.com>
2021-10-25 11:29:24 +0530

    🐛 FIX: User role is required

--- models/User.js
+++ models/User.js
@@ -4,18 +4,19 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: [true, 'Username is required'],
+    required: [true, 'Username is required!'],
     unique: true,
   },
   password: {
     type: String,
-    required: [true, 'Password is required'],
+    required: [true, 'Password is required!'],
     minlength: 8,
   },
   slug: String,
   role: {
     type: String,
     enum: ['mod', 'admin'],
+    required: [true, 'User role is required!'],
   },
   postsMarked: [
     {

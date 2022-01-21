8b11f5af59d2eac55f7b23d27e69a73a4ab33ab9
Srikar KSV <srikar1awesome@gmail.com>
2022-01-21 20:41:19 +0530

    📦 NEW: Added event handling to get paginated confessions

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,11 +1,99 @@
-const tables = document.querySelectorAll('table');
+const allConfessionContainer = document.querySelector(
+  '.dashboard__all-confessions'
+);
 
-function handleDashboardBtns(e) {
-  const button = e.target.classList;
+// Holds the current paginated confessions
+let allConfessions;
 
-  if (button.contains('btn-delete')) {
-    console.log('DELETE');
+const allConfessionTableTemplate = /* html */ `
+  <div class="dashboard__table-container">
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Marked by</th>
+          <th>Edit</th>
+          <th>Mark as spam</th>
+        </tr>
+      </thead>
+    </table>
+    <button class="btn btn-grey btn-prev">Previous</button>
+    <button class="btn btn-grey btn-next">Next</button>
+  </div>
+`;
+
+// Convert template to document fragment
+const allConfessionTableFragment = document
+  .createRange()
+  .createContextualFragment(allConfessionTableTemplate);
+
+allConfessionContainer.appendChild(allConfessionTableFragment);
+const allConfessionTable = allConfessionContainer.querySelector('table');
+const prevPageBtn = allConfessionContainer.querySelector('.btn-prev');
+const nextPageBtn = allConfessionContainer.querySelector('.btn-next');
+
+async function fetchAllConfessions(page) {
+  let url = '/api/confessions';
+  if (page) url += `?page=${page}`;
+  const response = await fetch(url, {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const result = await response.json();
+  return result;
+}
+
+function setPageBtnVisibility(page, pageBtn) {
+  // Resetting visibility
+  pageBtn.classList.remove('hide');
+
+  // Setting visibility based on the existence of prev or next page
+  if (page) {
+    pageBtn.setAttribute('data-page', page);
+  } else {
+    pageBtn.classList.add('hide');
   }
 }
 
-tables.forEach((table) => table.addEventListener('click', handleDashboardBtns));
+function updateAllConfessions() {
+  const { confessions, nextPage, prevPage } = allConfessions;
+  const tbody = document.createElement('tbody');
+
+  setPageBtnVisibility(prevPage, prevPageBtn);
+  setPageBtnVisibility(nextPage, nextPageBtn);
+
+  const html = confessions
+    .map(
+      ({ title, markedByStaff, slug, id }) => /* html */ `<tr>
+      <td><a href="/confessions/${slug}">${title}</a></td>
+      <td>${
+        markedByStaff
+          ? `<a href="/profile/${markedByStaff}">${markedByStaff}</a>`
+          : 'None'
+      }</td>
+      <td><a class="btn btn-yellow" href="/edit/${id}">Edit </a></td>
+      <td><button class="btn btn-purple" data-id="${id}">Mark</button></td>
+      </tr>`
+    )
+    .join('');
+
+  tbody.innerHTML = html;
+  allConfessionTable.querySelector('tbody')?.remove();
+  allConfessionTable.insertAdjacentElement('beforeend', tbody);
+}
+
+async function fetchNewPage(e) {
+  allConfessions = await fetchAllConfessions(e.target.dataset.page);
+  allConfessionTable.dispatchEvent(new CustomEvent('update'));
+}
+
+prevPageBtn.addEventListener('click', fetchNewPage);
+nextPageBtn.addEventListener('click', fetchNewPage);
+allConfessionTable.addEventListener('update', updateAllConfessions);
+
+// Load all confessions when DOMContentLoaded
+window.addEventListener('DOMContentLoaded', async () => {
+  allConfessions = await fetchAllConfessions();
+  allConfessionTable.dispatchEvent(new CustomEvent('update'));
+});

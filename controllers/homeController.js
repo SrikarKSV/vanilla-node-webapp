7899a3b85c4c9a9d320fd229f6b6ad4b39350923
Srikar KSV <srikar1awesome@gmail.com>
2022-01-22 20:03:41 +0530

    👌 IMPROVE: Styled new and edit form

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,11 +3,10 @@ const Confession = require('../models/Confession');
 exports.home = async (req, res) => {
   const top5Confessions = await Confession.getTop5Confessions();
   res.render('index', {
-    name: 'Srikar',
     top5Confessions,
   });
 };
 
 exports.newConfession = (req, res) => {
-  res.render('new-confession');
+  res.render('new-confession', { title: 'Write a confession' });
 };

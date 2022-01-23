70815d02f4c343ba6ef4fa3e3816c96a33cd3d29
Srikar KSV <srikar1awesome@gmail.com>
2022-01-23 21:47:36 +0530

    👌 IMPROVE: Added burger nav for smaller devices

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,19 @@
+const hamburger = document.querySelector('.hamburger');
+const closeBtn = document.querySelector('.close');
+const navbar = document.querySelector('nav');
+
+function toggleSideBar() {
+  navbar.classList.toggle('show');
+}
+
+function closeSideBar(e) {
+  if (e?.key === 'Escape') navbar.classList.remove('show');
+}
+
+hamburger.addEventListener('click', toggleSideBar);
+closeBtn.addEventListener('click', toggleSideBar);
+window.addEventListener('keyup', closeSideBar);
+
 // Load color generator when new confession page is open
 (async function () {
   if (window.location.pathname === '/new') {
